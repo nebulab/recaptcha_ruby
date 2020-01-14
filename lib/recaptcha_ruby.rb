b0c46e9007035542b231b06564ca152dbbac189b
Julie Marin <julie.sylvana.marin@gmail.com>
2020-01-13 23:11:39 -0500

    Tidy up repo name references

--- lib/recaptcha_ruby.rb
+++
@@ -1,6 +0,0 @@
-require "recaptcha_ruby/version"
-
-module RecaptchaRuby
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/recaptcha_ruby/version.rb
+++
@@ -1,3 +0,0 @@
-module RecaptchaRuby
-  VERSION = "0.1.0"
-end
